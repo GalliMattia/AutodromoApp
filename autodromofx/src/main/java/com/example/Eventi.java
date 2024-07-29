5e1f4507c812d78cea7f76744d98b925c5502e37
Mattia Galli <mattia.galli9@studio.unibo.it>
2024-07-29 11:39:57 +0200

    commit

--- autodromofx/src/main/java/com/example/Eventi.java
+++ autodromofx/src/main/java/com/example/Eventi.java
@@ -1,7 +1,29 @@
 package com.example;
 
+import java.time.LocalDate;
+
 public class Eventi {
-    //private int ID;
-    //private String Name;
-    //data
+    private int id;
+    private String name;
+    private LocalDate date;
+
+    public Eventi(int id, String name, LocalDate date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    
 }
+
